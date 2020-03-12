f7b80e1913f4d9490020ce09ba6d3b3b75087b7d
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2020-03-12 22:02:01 +0000

    Implement scrolling with ctrl+d and ctrl-u

--- lib/tefter_cli/views/components/cursor.ex
+++ lib/tefter_cli/views/components/cursor.ex
@@ -4,14 +4,19 @@ defmodule TefterCli.Views.Components.Cursor do
   """
 
   import Ratatouille.Constants, only: [key: 1]
+  alias TefterCli.Views.Components.Pagination
 
   @up [key(:arrow_up), key(:mouse_wheel_up), key(:ctrl_k), key(:ctrl_p)]
   @down [key(:arrow_down), key(:mouse_wheel_down), key(:ctrl_j), key(:ctrl_n)]
+  @next_page [key(:ctrl_d)]
+  @previous_page [key(:ctrl_u)]
 
   def update(state, msg, %{type: type}) do
     cursor = state[type][:cursor]
     resources_type = if TefterCli.Command.filter?(state), do: :filtered, else: :resources
     resources = state[type][resources_type]
+    scroll_size = round(Pagination.page_size() / 2)
+    resource_len = length(resources)
 
     case msg do
       _ when resources == [] ->
@@ -21,12 +26,24 @@ defmodule TefterCli.Views.Components.Cursor do
         put_in(state, [type, :cursor], cursor - 1)
 
       {:event, %{key: k}} when k in @up ->
-        put_in(state, [type, :cursor], length(resources) - 1)
+        put_in(state, [type, :cursor], resource_len - 1)
 
-      {:event, %{key: k}} when k in @down and cursor < length(resources) - 1 ->
+      {:event, %{key: k}} when k in @down and cursor < resource_len - 1 ->
         put_in(state, [type, :cursor], cursor + 1)
 
       {:event, %{key: k}} when k in @down ->
+        put_in(state, [type, :cursor], 0)
+
+      {:event, %{key: k}} when k in @next_page and cursor + scroll_size >= resource_len - 1 ->
+        put_in(state, [type, :cursor], resource_len - 1)
+
+      {:event, %{key: k}} when k in @next_page ->
+        put_in(state, [type, :cursor], cursor + scroll_size)
+
+      {:event, %{key: k}} when k in @previous_page and cursor - scroll_size >= 0 ->
+        put_in(state, [type, :cursor], cursor - scroll_size)
+
+      {:event, %{key: k}} when k in @previous_page ->
         put_in(state, [type, :cursor], 0)
 
       _ ->
